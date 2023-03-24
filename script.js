f6f6d2b5c9ea5c08478bcca0358ecc518cc9b595
Arpit Yadav <sinasa88@gmail.com>
2023-03-24 22:52:09 +0100

    After adding  select elements dynamically and images

--- script.js
+++ script.js
@@ -89,7 +89,23 @@ selectBrand.addEventListener("change", function () {
     <option class="options" value="ferrari812">Ferrari 812 Superfast</option>
     <option class="options" value="ferrariF8">Ferrari F8 Tributo</option>
     <option class="options" value="ferrarilaFerrari">Ferrari LaFerrari</option>
-    <option class="options" value="frerrariroma">Ferrari Roma</option>`;
+    <option class="options" value="ferrariroma">Ferrari Roma</option>`;
+
+    selectModal.addEventListener("change", function () {
+      const selectedModal = selectModal.value;
+
+      if (selectedModal === "ferrari488") {
+        formImg.src = "/img/ferrari488.png";
+      } else if (selectedModal === "ferrari812") {
+        formImg.src = "/img/ferrari812.png";
+      } else if (selectedModal === "ferrariF8") {
+        formImg.src = "/img/Ferrari-F8-Tributo.png";
+      } else if (selectedModal === "ferrarilaFerrari") {
+        formImg.src = "/img/FerrariLaFerrari.png";
+      } else if (selectedModal === "ferrariroma") {
+        formImg.src = "/img/Ferrari-Roma.png";
+      }
+    });
   } else if (selectedValue === "ford") {
     selectModal.innerHTML = `
     <option class="options" value="choose" selected>SELECT MODAL</option>
@@ -117,10 +133,25 @@ selectBrand.addEventListener("change", function () {
     selectModal.innerHTML = `
     <option class="options" value="choose" selected>SELECT MODAL</option>
     <option class="options" value="911">Porsche 911 GT3</option>
-    <option class="options" value="GT2">Porsche 911 GT2 RS</option>
+    <option class="options" value="taycan">Porsche Taycan</option>
     <option class="options" value="718">Porsche 718 Cayman GT4</option>
     <option class="options" value="718spyder">Porsche 718 Spyder</option>
     <option class="options" value="panamera">Porsche Panamera Turbo S </option>`;
+    selectModal.addEventListener("change", function () {
+      const selectedModal = selectModal.value;
+
+      if (selectedModal === "911") {
+        formImg.src = "/img/911gt3.png";
+      } else if (selectedModal === "taycan") {
+        formImg.src = "/img//Porsche-Taycan.png";
+      } else if (selectedModal === "718") {
+        formImg.src = "/img/cayman.png";
+      } else if (selectedModal === "718spyder") {
+        formImg.src = "/img/spyder.png";
+      } else if (selectedModal === "panamera") {
+        formImg.src = "/img/Porsche-Panamera-PNG-Image.png";
+      }
+    });
   } else if (selectedValue === "mclaren") {
     selectModal.innerHTML = `
     <option class="options" value="choose" selected>SELECT MODAL</option>
@@ -129,14 +160,44 @@ selectBrand.addEventListener("change", function () {
     <option class="options" value="570s">McLaren 570S</option>
     <option class="options" value="senna">McLaren Senna</option>
     <option class="options" value="mcLarengt">McLaren GT</option>`;
+    selectModal.addEventListener("change", function () {
+      const selectedModal = selectModal.value;
+
+      if (selectedModal === "p1") {
+        formImg.src = "/img/p1.png";
+      } else if (selectedModal === "720s") {
+        formImg.src = "/img/720s.png";
+      } else if (selectedModal === "570s") {
+        formImg.src = "/img/570.png";
+      } else if (selectedModal === "senna") {
+        formImg.src = "/img/senna.png";
+      } else if (selectedModal === "mcLarengt") {
+        formImg.src = "/img/mclarengt.png";
+      }
+    });
   } else if (selectedValue === "mercedes") {
     selectModal.innerHTML = `
     <option class="options" value="choose" selected>SELECT MODAL</option>
-    <option class="options" value="sclass">Mercedes-Benz S-Class</option>
-    <option class="options" value="amggt">Mercedes-Benz AMG GT</option>
-    <option class="options" value="gclass">Mercedes-Benz G-Class</option>
-    <option class="options" value="cls">Mercedes-Benz CLS-Class</option>
-    <option class="options" value="eclass">Mercedes-Benz E-Class</option>`;
+    <option class="options" value="bentgt">Mercedes GT</option>
+    <option class="options" value="sclass">Mercedes S-Class</option>
+    <option class="options" value="gclass">Mercedes G-Class</option>
+    <option class="options" value="amggt">Mercedes AMG GT</option>
+    <option class="options" value="cls">Mercedes CLS-Class</option>`;
+    selectModal.addEventListener("change", function () {
+      const selectedModal = selectModal.value;
+
+      if (selectedModal === "sclass") {
+        formImg.src = "/img/sclass.png";
+      } else if (selectedModal === "amggt") {
+        formImg.src = "/img/amg.png";
+      } else if (selectedModal === "gclass") {
+        formImg.src = "/img/gclass.png";
+      } else if (selectedModal === "cls") {
+        formImg.src = "/img/CLS-Class.png";
+      } else if (selectedModal === "bentgt") {
+        formImg.src = "/img/benzgt.png";
+      }
+    });
   } else if (selectedValue === "rolls-royce") {
     selectModal.innerHTML = `
     <option class="options" value="choose" selected>SELECT MODAL</option>
@@ -145,5 +206,20 @@ selectBrand.addEventListener("change", function () {
     <option class="options" value="wraith">Rolls-Royce Wraith</option>
     <option class="options" value="dawn">Rolls-Royce Dawn</option>
     <option class="options" value="cullinan">Rolls-Royce Cullinan</option>`;
+    selectModal.addEventListener("change", function () {
+      const selectedModal = selectModal.value;
+
+      if (selectedModal === "phantom") {
+        formImg.src = "/img/phantom2.png";
+      } else if (selectedModal === "ghost") {
+        formImg.src = "/img/ghost.png";
+      } else if (selectedModal === "wraith") {
+        formImg.src = "/img/wraith.png";
+      } else if (selectedModal === "dawn") {
+        formImg.src = "/img/dawn.png";
+      } else if (selectedModal === "cullinan") {
+        formImg.src = "/img/cullian.png";
+      }
+    });
   }
 });
